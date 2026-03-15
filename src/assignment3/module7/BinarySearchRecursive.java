6663330a0b48cff88ac7a40650b7a22fa2777013
Priyamurthy13 <murthypriyanka1@gmail.com>
2026-03-15 21:58:31 +0530

    initial

---
+++ src/assignment3/module7/BinarySearchRecursive.java
@@ -0,0 +1,25 @@
+package assignment3.module7;
+public class BinarySearchRecursive {
+
+    static int binarySearch(int[] arr,int left,int right,int target){
+        if(left > right)
+            return -1;
+
+        int mid = (left+right)/2;
+
+        if(arr[mid] == target)
+            return mid;
+        else if(arr[mid] > target)
+            return binarySearch(arr,left,mid-1,target);
+        else
+            return binarySearch(arr,mid+1,right,target);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6};
+
+        int result = binarySearch(arr,0,arr.length-1,4);
+
+        System.out.println("Index: " + result);
+    }
+}
---
+++ src/assignment3/module7/SortComplexity.java
@@ -0,0 +1,11 @@
+package assignment3.module7;
+public class SortComplexity {
+    public static void main(String[] args) {
+
+        System.out.println("Bubble Sort: Time O(n^2), Space O(1)");
+        System.out.println("Selection Sort: Time O(n^2), Space O(1)");
+        System.out.println("Insertion Sort: Time O(n^2), Space O(1)");
+        System.out.println("Merge Sort: Time O(n log n), Space O(n)");
+        System.out.println("Quick Sort: Time O(n log n) average, O(n^2) worst");
+    }
+}
